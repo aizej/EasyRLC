52345c62ec195a2f0876d62ee77047ca49488e32
aizej <davidxojd@gmail.com>
2025-05-09 09:41:53 +0200

    proper cleanup

--- app/src/main/java/com/aizej/easyrlc/MainActivity.kt
+++ app/src/main/java/com/aizej/easyrlc/MainActivity.kt
@@ -30,7 +30,15 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
+import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -66,7 +74,6 @@ import kotlin.math.pow
 import kotlin.math.roundToInt
 import kotlin.math.sign
 import kotlin.math.sqrt
-import com.aizej.easyrlc.R
 
 class MainActivity : ComponentActivity() {
     var component_num_R = 1
