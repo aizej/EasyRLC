7282385632e0e447a8d4e1f85d6454696c22db1b
aizej <davidxojd@gmail.com>
2025-07-21 15:45:03 +0200

    -decreasing SDK version for compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.aizej.easyrlc"
         minSdk = 28
         targetSdk = 35
-        versionCode = 13
-        versionName = "1.13"
+        versionCode = 14
+        versionName = "1.14"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
